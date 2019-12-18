a30f91fe00ed56b815a91f10b03a81067f8847d0
Marco <marco.rinaldi9@live.com>
2019-12-18 12:52:11 +0100

    Added flex layout and added first schematics of angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,30 @@ import { AppComponent } from './app.component';
 import { CurrentWeatherComponent } from './components/current-weather/current-weather.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { SideNavComponent } from './components/side-nav/side-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
-	declarations: [AppComponent, CurrentWeatherComponent],
-	imports: [BrowserModule, AppRoutingModule, HttpClientModule, BrowserAnimationsModule],
+	declarations: [AppComponent, CurrentWeatherComponent, SideNavComponent],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		HttpClientModule,
+		BrowserAnimationsModule,
+		FlexLayoutModule,
+		LayoutModule,
+		MatToolbarModule,
+		MatButtonModule,
+		MatSidenavModule,
+		MatIconModule,
+		MatListModule
+	],
 	providers: [],
 	bootstrap: [AppComponent]
 })
